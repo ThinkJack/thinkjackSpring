8dacb759e654ee586d2586dabb74e0fe2446a3b2
gwiyeom <gwiyeom2013@gmail.com>
2017-12-08 21:39:57 +0900

    2017.12.8 댓글기능 test

--- src/main/java/domain/ReplyVO.java
+++ src/main/java/domain/ReplyVO.java
@@ -12,15 +12,69 @@ public class ReplyVO {
 //	댓글 내용
 	private String replyWirter;
 //	유저 ID
-//	private Timestamp replyRegdate;
-////Timestamp를 써야하는지 date를 써야하는지 모르겠음
-//	private Timestamp replyUpdate;
 	private Integer replyParent;
 //	대댓글에 필요한 부분
 	private Integer replyLikecnt;
 //좋아요수
-	
 
-	
-	
+
+	public Integer getReplyId() {
+		return replyId;
+	}
+
+	public void setReplyId(Integer replyId) {
+		this.replyId = replyId;
+	}
+
+	public Integer getBoardId() {
+		return boardId;
+	}
+
+	public void setBoardId(Integer boardId) {
+		this.boardId = boardId;
+	}
+
+	public String getReplyText() {
+		return replyText;
+	}
+
+	public void setReplyText(String replyText) {
+		this.replyText = replyText;
+	}
+
+	public String getReplyWirter() {
+		return replyWirter;
+	}
+
+	public void setReplyWirter(String replyWirter) {
+		this.replyWirter = replyWirter;
+	}
+
+	public Integer getReplyParent() {
+		return replyParent;
+	}
+
+	public void setReplyParent(Integer replyParent) {
+		this.replyParent = replyParent;
+	}
+
+	public Integer getReplyLikecnt() {
+		return replyLikecnt;
+	}
+
+	public void setReplyLikecnt(Integer replyLikecnt) {
+		this.replyLikecnt = replyLikecnt;
+	}
+
+	@Override
+	public String toString() {
+		return "ReplyVO{" +
+				"replyId=" + replyId +
+				", boardId=" + boardId +
+				", replyText='" + replyText + '\'' +
+				", replyWirter='" + replyWirter + '\'' +
+				", replyParent=" + replyParent +
+				", replyLikecnt=" + replyLikecnt +
+				'}';
+	}
 }
